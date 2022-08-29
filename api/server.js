02b1708387c8ddd15fb292c98dd3121d716b49e5
jeff kang <jeffyjkang@gmail.com>
2022-08-29 13:37:09 -0500

    init

--- api/server.js
+++ api/server.js
@@ -1,3 +1,12 @@
 // BUILD YOUR SERVER HERE
+const express = require('express');
 
-module.exports = {}; // EXPORT YOUR SERVER instead of {}
+const server = express();
+server.use(express.json());
+
+server.get("/", (req,res) => {
+  res.json({message: "hello world"})
+})
+
+module.exports = server; // EXPORT YOUR SERVER instead of {}
+
